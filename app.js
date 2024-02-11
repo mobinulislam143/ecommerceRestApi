7ae8d7ada2f4e11a498b2bd7c744eedf12f58d9c
Mobinul Islam Mahi <115027489+mobinulislam143@users.noreply.github.com>
2024-02-11 23:38:33 +0600

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const mongoose =require('mongoose');
 app.use(cookieParser())
 app.use( 
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:7000",
   })
 )
 app.use(helmet())
