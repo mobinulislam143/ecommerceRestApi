91b0582bc1c4d2cee6e0b3c00a75e7324ef62d5a
Mobinul Islam Mahi <115027489+mobinulislam143@users.noreply.github.com>
2024-05-02 10:11:57 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// basic import
 const express = require('express');
-const app = new express();
+const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
-// Security Middleware Lib Import
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const hpp = require('hpp');
@@ -13,21 +11,12 @@ const xss = require('xss-clean');
 const cors = require('cors');
 const mongoSanitize = require('express-mongo-sanitize');
 
-// Database Lib Import
 const mongoose = require('mongoose');
 
-// Security Middleware Implement
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ['https://66330f787d8336631139d715--scintillating-peony-3c6b03.netlify.app/', 'https://h-mart-api.onrender.com'],
-    credentials: true,
-  })
-);
-
+app.use(cors()); // Updated CORS configuration
 app.options('*', cors());
 
-// Error handling middleware specifically for CORS-related errors
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
     res.status(401).json({ error: 'Unauthorized request' });
@@ -50,7 +39,6 @@ app.use(bodyParser.json());
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 });
 app.use(limiter);
 
-// Routing Implement
 const appRouter = require('./src/Routes/api');
 app.use('/api', appRouter);
 
@@ -74,7 +62,6 @@ mongoose
     console.log(e);
   });
 
-// frontend connect
 app.use(express.static('client/dist'));
 app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
